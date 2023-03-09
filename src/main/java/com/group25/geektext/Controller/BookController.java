5eed56c20a716e5b3d5c48cc2b5c61e1397f56ee
Casey Sit <caseysit0714@gmail.com>
2023-03-09 13:15:46 -0500

    Added genre, bestseller, and rating get req

--- src/main/java/com/group25/geektext/Controller/BookController.java
+++ src/main/java/com/group25/geektext/Controller/BookController.java
@@ -3,12 +3,11 @@ package com.group25.geektext.Controller;
 import com.group25.geektext.Models.Book;
 import com.group25.geektext.Repo.BookRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping(path="/book") // after Application, /book
@@ -18,7 +17,7 @@ public class BookController {
 
     @PostMapping(path="/addBook") // Only POST req
     public @ResponseBody String addBook (@RequestParam String isbn
-                                        , @RequestParam String name,
+                                        ,@RequestParam String name,
                                          @RequestParam String desc,
                                          @RequestParam Double price,
                                          @RequestParam String author,
@@ -49,6 +48,55 @@ public class BookController {
     @GetMapping(path="/all") //get books
     public @ResponseBody Iterable<Book> getAllBooks() {
         // JSON with all books
-        return bookRepo.findAll();
+        //return bookRepo.findAll();
+        return bookRepo.findAllBooks();
     }
+
+    @GetMapping(path="/genre")
+    public @ResponseBody Iterable<Book> getBooksByGenre(@RequestParam String genre){
+        return bookRepo.findBooksByGenre(genre);
+    }
+
+    @GetMapping(path="/bestsellers")
+    public @ResponseBody Iterable<Book> getBooksbyCopiesSold(){
+        return bookRepo.findBooksbyCopiesSold().subList(0,10);
+    }
+
+    @GetMapping(path="/ratings")
+    public @ResponseBody Iterable<Book> getBooksbyRatings(@RequestParam double rating){
+        return bookRepo.findBooksByRatings(rating);
+    }
+
+
+    /*TODO: Debug this*/
+
+//    @PatchMapping(path="/updatebooks/{id}")
+//    public @ResponseBody ResponseEntity<Book> updateBooksByPublisher(@PathVariable int id, @RequestParam String pub,
+//                                                                     @RequestParam int discount)
+//    {
+//        updateBookByPublisher(pub, discount);
+//    }
+
+//    private void updateBookByPublisher(String pub, int discount){
+//        bookRepo.updateBookByPublisher(pub, discount);
+//    }
+
+//    @PatchMapping(path="/updatebooks/{pub:[a-zA-Z &+-]*}", consumes = "application/json-patch+json")
+//    public ResponseEntity<Book> updatePricesByPublisher(@PathVariable String pub,
+//                                                                        @RequestBody JsonPatch patch)
+//    {
+//        Book b = bookRepo.findBookByPublisher(pub);=-
+//        Book bPatched = applyPatchToBook(patch, b);
+//        return ResponseEntity.ok(bPatched);
+////        for(Book x:bookRepo.findAll()){
+////            if(x.getPublisher().equalsIgnoreCase(pub)){
+////                x.setPrice(x.getPrice() - ((discount/100) * x.getPrice()));
+////                bookRepo.save(x);
+////            }
+////
+////        }
+////        //bookRepo.updateBooksByPublisher(pub, discount);
+////        return new ResponseEntity<String>("Update Success", HttpStatus.OK);
+//    }
+
 }
--- src/main/java/com/group25/geektext/Repo/BookRepo.java
+++ src/main/java/com/group25/geektext/Repo/BookRepo.java
@@ -1,11 +1,37 @@
 package com.group25.geektext.Repo;
 
 import com.group25.geektext.Models.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface BookRepo extends CrudRepository<Book, Integer> {
+public interface BookRepo extends JpaRepository<Book, Integer> {
+
+    @Query(value = "SELECT * FROM book", nativeQuery = true)
+    List<Book> findAllBooks();
+
+    @Query(value = "SELECT * FROM book WHERE genre=:genre", nativeQuery = true)
+    List<Book> findBooksByGenre(@Param("genre") String genre);
+
+    @Query(value="SELECT * FROM book ORDER BY copies_sold DESC", nativeQuery=true)
+    List<Book> findBooksbyCopiesSold();
+
+    @Query(value="SELECT * FROM book WHERE rating >= :rating", nativeQuery = true)
+    List<Book> findBooksByRatings(@Param("rating") double rating);
+
+    /*TODO: This is not working. Method not Allowed?*/
+    @Modifying
+    @Query(value="UPDATE book SET price = price - ((:discount/100) * price) WHERE publisher=:publisher",
+            nativeQuery = true)
+    void updateBookByPublisher(@Param("publisher") String publisher, @Param("discount") int discount);
+
+    @Query(value="SELECT * FROM book WHERE publisher=:publisher", nativeQuery = true)
+    Book findBookByPublisher(@Param("publisher") String publisher);
 
 }
