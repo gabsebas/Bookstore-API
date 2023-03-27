f921efaff7a3167a5fb997a20207bbf044f6b3b9
Fran <101612978+fscac001@users.noreply.github.com>
2023-03-27 15:32:52 -0400

    added cart repo

---
+++ src/main/java/com/group25/geektext/Repo/CartRepository.java
@@ -0,0 +1,21 @@
+package com.group25.geektext.Repo;
+
+import com.group25.geektext.Models.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CartRepository extends MongoRepository<Cart, String> {
+
+    @Query("{'id': ?0}")
+    Optional<Cart> findById(String id);
+
+    @Query("{'userid': ?0}")
+ 
+
+
+  
