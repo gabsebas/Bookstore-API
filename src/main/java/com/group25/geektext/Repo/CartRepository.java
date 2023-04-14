46431a4ccb8db2e504fc440575877b9059bdd0c1
Fran <101612978+fscac001@users.noreply.github.com>
2023-04-13 23:38:49 -0400

    Update CartRepository.java

--- src/main/java/com/group25/geektext/Repo/CartRepository.java
+++ src/main/java/com/group25/geektext/Repo/CartRepository.java
@@ -2,9 +2,7 @@ package com.group25.geektext.Repo;
 
 import com.group25.geektext.Models.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
